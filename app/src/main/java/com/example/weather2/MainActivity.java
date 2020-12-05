556834ef075332a11ca4ce0f1213b969a3e3f7a5
andriybobchuk <andriybobchuk@gmail.com>
2020-12-05 15:32:42 +0100

    before cleaning everything out

--- app/src/main/java/com/example/weather2/MainActivity.java
+++ app/src/main/java/com/example/weather2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weather2;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_sunrise, tv_sunset;
 
     ImageButton btn_options, btn_back_from_options;
+
+    Button btn_day0,btn_day1,btn_day2,btn_day3,btn_day4,btn_day5,btn_day6;
+    //int dayIndex = 0; // Declaration & init of the 'default_day_index' (0 is 'today')
 
 
     public void open()
@@ -71,7 +75,119 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+//        btn_day0 = findViewById((R.id.btn_day0));
+//        btn_day0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // The following construction is for selecting & deselecting with click, but...
+//                // We need smth else
+////                if (btn_day0.isSelected())
+////                {
+////                    btn_day0.setSelected(false);
+////                } else
+////                {
+////                    btn_day0.setSelected(true);
+////                }
+//
+//                //Smth like that
+//                btn_day0.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day6.setSelected(false);
+//
+//                getForecast ();
+//
+//            }
+//        });
+//        btn_day1 = findViewById((R.id.btn_day1));
+//        btn_day1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day1.setSelected(true);
+//
+//                btn_day0.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day6.setSelected(false);
+//            }
+//        });
+//        btn_day2 = findViewById((R.id.btn_day2));
+//        btn_day2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day2.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day0.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day6.setSelected(false);
+//            }
+//        });
+//        btn_day3 = findViewById((R.id.btn_day3));
+//        btn_day3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day3.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day0.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day6.setSelected(false);
+//            }
+//        });
+//        btn_day4 = findViewById((R.id.btn_day4));
+//        btn_day4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day4.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day0.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day6.setSelected(false);
+//            }
+//        });
+//        btn_day5 = findViewById((R.id.btn_day5));
+//        btn_day5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day5.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day0.setSelected(false);
+//                btn_day6.setSelected(false);
+//            }
+//        });
+//        btn_day6 = findViewById((R.id.btn_day6));
+//        btn_day6.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btn_day6.setSelected(true);
+//
+//                btn_day1.setSelected(false);
+//                btn_day2.setSelected(false);
+//                btn_day3.setSelected(false);
+//                btn_day4.setSelected(false);
+//                btn_day5.setSelected(false);
+//                btn_day0.setSelected(false);
+//            }
+//        });
+//
 
     }
 
@@ -82,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
     {
 
         //API Call
-        String URL = "https://api.openweathermap.org/data/2.5/onecall?lat=50.29761&lon=18.67658&exclude=current,minutely,hourly&appid=ace729200f31ff6473436ef39ad854ea&units=metric&lang=en";
+        String URL = "https://api.openweathermap.org/data/2.5/onecall?lat=50.29761&lon=18.67658&exclude=minutely,hourly&appid=ace729200f31ff6473436ef39ad854ea&units=metric&lang=en";
 
 
         JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
@@ -91,6 +207,9 @@ public class MainActivity extends AppCompatActivity {
 
                 try {
 
+                    /* ==========================================================================================================*/
+                    /* |                                  1-GET JSON ARRAY AND ITS OBJECTS                                       */
+                    /* ==========================================================================================================*/
                     // Get the array of daily data from JSON Array.
                     // It looks like: 'array "daily"[{day0(today)}{day1}{day2}{day3}]'
                     // Each of day1,day2,day3 has its fields 'temp','pressure','humm' etc;
@@ -101,6 +220,7 @@ public class MainActivity extends AppCompatActivity {
 
                     // Getting the day1 (TOMORROW) object from the daily array.
                     JSONObject day01 = dailyArray.getJSONObject(1);
+                    tv_temperature.setText(day01.getDouble("temp")+"°C");
 
                     // And so on...
                     JSONObject day02 = dailyArray.getJSONObject(2);
@@ -112,17 +232,47 @@ public class MainActivity extends AppCompatActivity {
                     // Now we got all of the days needed.
 
                     //Getting JSON objects
-//                    JSONObject obj_current = response.getJSONObject("current");//
+                    JSONObject obj_current = response.getJSONObject("current");//
 //                    JSONObject obj_wind = response.getJSONObject("wind");//for "speed"
 //                    JSONObject obj_sys = response.getJSONObject("sys");//for "sunrise\sunset"
-//                    JSONArray array = response.getJSONArray("weather");//for "description"
-//                    JSONObject obj = array.getJSONObject(0);
+                    JSONArray array = response.getJSONArray("weather");//for "description"
+                    JSONObject obj = array.getJSONObject(0);
 //
 //
-//                    //Assigning STRING variables (From TOP to BOTTOM)
-//                    String city = response.getString("name");//From response
-//
-//                    String temperature = String.valueOf(obj_current.getInt("temp"));
+
+
+
+
+                    /* ==========================================================================================================*/
+                    /* |                                  2-ASSIGN STRINGS USING JSON OBJECTS                                    */
+                    /* ==========================================================================================================*/
+
+                    //Assigning STRINGs (TOP to BOTTOM)
+                    String city = "[Gliwice]";
+                    tv_region.setText(city);
+
+                  //  String temperature = String.valueOf(obj_current.getInt("temp"));
+                    //String temperature = String.valueOf(day0.getInt("temp"));
+
+
+
+
+
+                   // if(btn_day0.isSelected())
+                    //{
+
+                   // }
+
+                    /* ==========================================================================================================*/
+                    /* |                                  3-Set labels                                                           */
+                    /* ==========================================================================================================*/
+
+
+
+
+
+
+
 //                    String temp_min = String.valueOf(obj_current.getInt("temp_min"));
 //                    String temp_max = String.valueOf(obj_current.getInt("temp_max"));
 //
@@ -138,6 +288,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+////////////////////////////////////////////
 
 
 
